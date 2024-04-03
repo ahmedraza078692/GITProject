69a9f2ce1a1f8cf7b77bf9f668d06299f549b0ed
ahmedraza078692 <ahmedraza078692@gmail.com>
2024-04-04 00:28:51 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,18 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String Hungry = "YES";
+
+		if (Hungry == "YES")
+
+			System.out.println("Eat Something......");
+
+		else {
+			System.out.println("Do Work......");
+		}
+
+	}
+
+}
